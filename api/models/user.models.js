b57df232f866057bca6a17d426f3e5b716a23322
Nadeesha Sumanasekara <118651477+NadeeshaSumanasekara@users.noreply.github.com>
2023-12-13 08:49:51 +0530

    create a test api route

--- api/models/user.models.js
+++ api/models/user.models.js
@@ -20,6 +20,6 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const User = mongoose.model("User", username);
+const User = mongoose.model("User", userSchema);
 
 export default User;
