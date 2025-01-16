05f9d0c0c1cc100e79dfe5471c854f173b9403e6
Nadeesha Sumanasekara <118651477+NadeeshaSumanasekara@users.noreply.github.com>
2025-01-16 07:03:52 +0530

    corrected

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => {
+app.listen(3002, () => {
   console.log("Server is running on port 3000");
 });
 
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -13,7 +13,6 @@ export default function SignUp() {
       [e.target.id]: e.target.value,
     });
   };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -40,7 +39,6 @@ export default function SignUp() {
       setError(error.message);
     }
   };
-
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
@@ -66,16 +64,17 @@ export default function SignUp() {
           id="password"
           onChange={handleChange}
         />
+
         <button
           disabled={loading}
-          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:placeholder-opacity-95 disabled:placeholder-opacity-80"
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
         >
-          {loading ? "Loading..." : "Sign up"}
+          {loading ? "Loading..." : "Sign Up"}
         </button>
         <OAuth />
       </form>
       <div className="flex gap-2 mt-5">
-        <p>Having an account?</p>
+        <p>Have an account?</p>
         <Link to={"/sign-in"}>
           <span className="text-blue-700">Sign in</span>
         </Link>
