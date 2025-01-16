d46f1e972f988db10866966eebeb298d13ae9664
Nadeesha Sumanasekara <118651477+NadeeshaSumanasekara@users.noreply.github.com>
2025-01-16 17:01:23 +0530

    create user update api

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import useRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 mongoose
   .connect(process.env.MONGO)
@@ -17,6 +18,8 @@ mongoose
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3002, () => {
   console.log("Server is running on port 3000");
